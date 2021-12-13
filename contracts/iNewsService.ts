26e51ea38a530cf4824e5c8899365d2196df5a9c
ivansimplicio <ivansimplicio@outlook.com>
2021-12-13 20:12:05 -0300

    documenting the project with JSDoc

--- contracts/iNewsService.ts
+++ contracts/iNewsService.ts
@@ -1,5 +1,9 @@
 import { News } from './../models/news';
 import { IService } from './iService';
 
+/**
+ * Contrato INewsService
+ * @summary esse contrato implementa a interface IService passando a model de News
+ */
 export interface INewsService extends IService<News>{
 }
--- contracts/iVideosService.ts
+++ contracts/iVideosService.ts
@@ -1,5 +1,9 @@
 import { IService } from './iService';
 import { Videos } from "../models/videos";
 
+/**
+ * Contrato IVideosService
+ * @summary esse contrato implementa a interface IService passando a model de Podcast
+ */
 export interface IVideosService extends IService<Videos>{
 }
